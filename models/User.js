cef82eb5acfe873c01f01d7ef30dfe98be8d474e
gusuly0rum <donguk.kim1112@gmail.com>
2018-08-25 18:43:13 -0700

    Finish signup backend route and controller

--- models/User.js
+++ models/User.js
@@ -2,24 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true
-  },
-  lastname: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
+    unique: true,
     required: true
   },
   password: {
     type: String,
-    required: true
-  },
-  birthdate: {
-    type: Date,
     required: true
   },
   date: {
--- server.js
+++ server.js
@@ -5,21 +5,20 @@ const users = require('./routes/api/users');
 const database = require('./config/keys').mongoURI;
 
 const port = process.env.PORT || 3000;
-
-const app = express();
-app.use('/api/users', users);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 mongoose
   .connect(database, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(error => console.log(error));
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
-});
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/users', users);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
 });
