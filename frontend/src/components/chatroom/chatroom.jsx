8c193d0141d77ea35469386d4b7dbfa64bd96fef
Jason Wong <jsnwong@ucdavis.edu>
2018-08-28 21:22:05 -0700

    Add basic chatroom and send event listener

--- frontend/src/components/chatroom/chatroom.jsx
+++ frontend/src/components/chatroom/chatroom.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import io from "socket.io-client";
 
 class Chatroom extends React.Component {
   constructor(props){
     super(props);
+    this.socket = io.connect();
     this.state = {
-
+      message: ""
     };
+    this.handleSend = this.handleSend.bind(this);
 
   }
 
@@ -18,10 +21,32 @@ class Chatroom extends React.Component {
 
   handleSend(e){
     e.preventDefault();
+    this.socket.emit('chat message', document.getElementById('m').value);
+    document.getElementById('m').value = '';
+    return false;
   }
 
+  chatUpdate(){
+    this.socket.on('chat message', function (msg) {
+      let chatElem = document.createElement("li");
+      chatElem.append(msg);
+      document.getElementById('messages')
+        .append(chatElem);
+    });
+  }
+
+  
   render() {
-    return (<h1>Chatroom</h1>)
+    return (
+      <div>
+        <h1>Chatroom</h1>
+        <ul id="messages"></ul>
+        <form id='chat-form' onSubmit={this.handleSend}>
+          <input id="m" autocomplete="off" />
+          <button>Send</button>
+        </form>
+      </div>
+    )
   }
 
 }
