a6a46c1a5353eaa78cfe4fc776f4be02cc29ea53
Kevin Choy <kevinchoy22@gmail.com>
2018-08-28 22:15:47 -0700

    set current user

--- frontend/src/reducers/session_errors_reducer.js
+++ frontend/src/reducers/session_errors_reducer.js
@@ -1,12 +1,12 @@
 import { GET_ERRORS} from '../util/session_api_util';
-import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { SET_CURRENT_USER } from '../actions/session_actions';
 
 export default (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case GET_ERRORS:
       return action.payload;
-    case RECEIVE_CURRENT_USER:
+    case SET_CURRENT_USER:
       return [];
     default:
       return state;
