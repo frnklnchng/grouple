1385d3e4c26ec21aa2f6363d26b61ff7d181ad5c
Kevin Choy <kevinchoy22@gmail.com>
2018-09-02 22:19:54 -0700

    edited passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const User = require('../models/User');
-const keys = require('../config/keys');
+const keys = require('./keys.js');
 
 const options = {};
 options.secretOrKey = keys.secretOrKey;
