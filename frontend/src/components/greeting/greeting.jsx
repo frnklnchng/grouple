ef52c7bc4acf55bf58fe275f4fea65d277e4d83c
Kevin Choy <kevinchoy22@gmail.com>
2018-09-02 18:25:41 -0700

    trying to revert

--- frontend/src/components/greeting/greeting.jsx
+++ frontend/src/components/greeting/greeting.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Greeting = ({ currentUser, logout }) => {
   const sessionLinks = () => (
     <nav className="login-signup">
-      <Link to="/login">Login</Link>
+      <Link to="/login">Login!</Link>
       &nbsp;or&nbsp;
       <Link to="/signup">Sign up!</Link>
     </nav>
