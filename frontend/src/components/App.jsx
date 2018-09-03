7e92774c1757f5a93eb8d7db4df858d56b3eedfb
gusuly0rum <donguk.kim1112@gmail.com>
2018-09-02 22:25:31 -0700

    fix redirect

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { ProtectedRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import SplashContainer from './splash/splash_container';
 import GreetingContainer from './greeting/greeting_container';
 import ChatroomContainer from './chatroom/chatroom_container.js';
